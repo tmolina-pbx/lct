f973cdbb03606d26f0a5e25c869fa0bfbd39e9cd
tmolina-pbx <tito.molina@photobox.com>
2018-11-24 11:27:43 +0000

    Commit before ejecting

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./navigation.scss";
+
+export default class Navbar extends Component {
+  burgerToggle = function() {
+    let linksEl = document.querySelector(".narrowLinks");
+    if (linksEl.style.display === "block") {
+      linksEl.style.display = "none";
+    } else {
+      linksEl.style.display = "block";
+    }
+  };
+
+  render() {
+    return (
+      <nav>
+        <div className="navWide">
+          <div className="wideDiv">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div className="navNarrow">
+          <i className="fa fa-bars fa-2x" />
+          <div className="narrowLinks">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
